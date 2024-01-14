aee98161e1468af7585dd9841aa33c2bbc0706d8
Khalid <khalid@local.lan>
2024-01-14 18:22:26 -0500

    feat: show starships details on Modal

---
+++ src/Components/StarshipDetails.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+
+const renderListSection = (title, items) => {
+    if (items.length === 0) {
+      return null;
+    }
+  
+    return (
+      <div>
+        <p className="mt-3"><strong>{title}:</strong></p>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              <a href={item} target="_blank">
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+const StarshipDetails = ({ show, onHide, starship }) => {
+  if (!starship) {
+    return null;
+  }
+
+  const {
+    name,
+    cost_in_credits,
+    length,
+    max_atmosphering_speed,
+    crew,
+    passengers,
+    cargo_capacity,
+    consumables,
+    hyperdrive_rating,
+    MGLT,
+    pilots,
+    films,
+  } = starship;
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="container">
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Cost in Credits:</strong>
+            </div>
+            <div className="col-sm-4">{cost_in_credits}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Length:</strong>
+            </div>
+            <div className="col-sm-4">{length}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Max Atmosphering Speed:</strong>
+            </div>
+            <div className="col-sm-4">{max_atmosphering_speed}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Crew:</strong>
+            </div>
+            <div className="col-sm-4">{crew}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Passengers:</strong>
+            </div>
+            <div className="col-sm-4">{passengers}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Cargo Capacity:</strong>
+            </div>
+            <div className="col-sm-4">{cargo_capacity}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Consumables:</strong>
+            </div>
+            <div className="col-sm-4">{consumables}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>Hyperdrive Rating:</strong>
+            </div>
+            <div className="col-sm-4">{hyperdrive_rating}</div>
+            </div>
+            <div className="row mb-2">
+            <div className="col-sm-8">
+                <strong>MGLT:</strong>
+            </div>
+            <div className="col-sm-4">{MGLT}</div>
+            </div>
+
+
+            {renderListSection('Pilots', pilots)}
+            {renderListSection('Films', films)}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default StarshipDetails;
--- src/Components/StarshipsTable.js
+++ src/Components/StarshipsTable.js
@@ -1,43 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Table, Button } from 'react-bootstrap';
+import StarshipDetails from './StarshipDetails';
 
 const StarshipsTable = () => {
   const [starships, setStarships] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [selectedStarship, setSelectedStarship] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchStarships = async () => {
       try {
-        const response = await axios.get('https://swapi.dev/api/starships/');
-        setStarships(response.data.results);
+        const response = await axios.get('http://192.168.0.36:5000/api/starships');
+        setStarships(response.data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching starships:', error.message);
+        setLoading(false);
       }
     };
 
     fetchStarships();
   }, []);
 
+  const handleShowDetails = (starship) => {
+    setSelectedStarship(starship);
+    setShowDetails(true);
+  };
+
+  const handleCloseDetails = () => {
+    setShowDetails(false);
+  };
+
   return (
     <div className="container mt-4">
       <h2>Star Wars Starships</h2>
-      <table className="table table-responsive">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Model</th>
-            <th>Manufacturer</th>
-          </tr>
-        </thead>
-        <tbody>
-          {starships.map((starship) => (
-            <tr key={starship.name}>
-              <td>{starship.name}</td>
-              <td>{starship.model}</td>
-              <td>{starship.manufacturer}</td>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <Table className="table table-responsive">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Model</th>
+              <th>Manufacturer</th>
+              <th>Starship class</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {starships.map((starship) => (
+              <tr key={starship.name}>
+                <td>{starship.name}</td>
+                <td>{starship.model}</td>
+                <td>{starship.manufacturer}</td>
+                <td>{starship.starship_class}</td>
+                <td>
+                  <Button variant="primary" onClick={() => handleShowDetails(starship)}>
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+
+      <StarshipDetails show={showDetails} onHide={handleCloseDetails} starship={selectedStarship} />
     </div>
   );
 };
