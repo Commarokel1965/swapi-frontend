2b1586edbe7f6d3216a5911c65bcf060f1ae527f
Khalid <khalid@local.lan>
2024-01-14 17:53:02 -0500

    feat: setup request to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [starships, setStarships] = useState([]);
+
+  useEffect(() => {
+    const fetchStarships = async () => {
+      try {
+        const response = await axios.get('http://192.168.0.36:5000/api/starships');
+        setStarships(response.data);
+      } catch (error) {
+        console.error('Error fetching starships:', error.message);
+      }
+    };
+
+    fetchStarships();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Star Wars Starships</h1>
+      <ul>
+        {starships.map((starship) => (
+          <li key={starship.name}>{starship.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
